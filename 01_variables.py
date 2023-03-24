12af02c5d843433b0a4853f8ffe8eb183bcf4be1
Soy Leo <mustang.arg@gmail.com>
2023-03-24 03:24:28 -0300

    ok

---
+++ 01_variables.py
@@ -0,0 +1,39 @@
+# variables
+
+my_variable = ("Holaaaaa soy una variable!!")
+print (my_variable)
+
+my_int_variable =(5)
+print (my_int_variable)
+
+my_bool_variable =(True)
+print (my_bool_variable)
+
+print(my_bool_variable, 'pinta por aca',my_int_variable)
+
+var_texto = str(my_int_variable)
+print (var_texto)
+print (type (var_texto))
+
+
+# funsion LEN 
+print (len (my_variable))
+
+
+# Variables en una sola linea
+name, surname, alias, age = "leo","san","leogol16", 42
+print (name,surname,alias,age)
+
+"""
+first_name = input('What is your name: ')
+age = input('How old are you? ')
+
+print(first_name)
+print(age)
+"""
+
+
+ # frozando el tipo de variable
+address: str = "Mi direccion"
+address =32
+print (address)
